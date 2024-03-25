7f79767c209730cb3637f0118b1dd61d7b46cacc
AxelM74 <129221483+AxelM74@users.noreply.github.com>
2024-03-24 22:25:45 -0600

    Update StudentDAO.php

--- dao/StudentDAO.php
+++ dao/StudentDAO.php
@@ -43,77 +43,48 @@ final class StudentDAO extends ConexionDB
     }
 }
 
-public function insertStudent($student)
-{
-    $query = "INSERT INTO students (registration, name, p_last_name, m_last_name, gender, birthday_date, origin_place, date_of_registration) 
-              VALUES (:registration, :name, :p_last_name, :m_last_name, :gender, :birthday_date, :origin_place, :date_of_registration)";
-    $params = array(
-        ':registration' => $student->registration,
-        ':name' => $student->name,
-        ':p_last_name' => $student->p_last_name,
-        ':m_last_name' => $student->m_last_name,
-        ':gender' => $student->gender,
-        ':birthday_date' => $student->birthday_date,
-        ':origin_place' => $student->ethnicity,
-        ':date_of_registration' => $student->date_of_registration
-    );
-    $success = $this->executeQuery($query, $params);
-    return $success;
-}
-
-public function updateStudent($student)
-{
-    $query = "UPDATE students SET name = :name, p_last_name = :p_last_name, m_last_name = :m_last_name, gender = :gender, birthday_date = :birthday_date, origin_place = :origin_place, date_of_registration = :date_of_registration WHERE registration = :registration";
-    $params = array(
-        ':name' => $student->name,
-        ':p_last_name' => $student->p_last_name,
-        ':m_last_name' => $student->m_last_name,
-        ':gender' => $student->gender,
-        ':birthday_date' => $student->birthday_date,
-        ':origin_place' => $student->ethnicity,
-        ':date_of_registration' => $student->date_of_registration,
-        ':registration' => $student->registration
-    );
-    $success = $this->executeQuery($query, $params);
-    return $success;
-}
-
+// Ejemplo de uso GetAllWithPagination
+/*
 $studentDAO = new StudentDAO();
+$students = $studentDAO->GetAllWithPagination(2, 5);
+foreach ($students as $key => $student) {
+   echo "$key: {$student->name}\n";
 
-$updatedStudent = new Student();
-$updatedStudent->registration = " "; // El número de registro del estudiante que deseas actualizar
-$updatedStudent->name = " "; // Nuevo nombre
-$updatedStudent->p_last_name = " "; // Nuevo primer apellido
-$updatedStudent->m_last_name = " "; // Nuevo segundo apellido
-$updatedStudent->gender = " "; // Nuevo género
-$updatedStudent->birthday_date = " "; // Nueva fecha de nacimiento
-$updatedStudent->ethnicity = " "; // Nueva etnia
-$updatedStudent->date_of_registration = " "; // Nueva fecha de registro
-
-$updated = $studentDAO->updateStudent($updatedStudent);
-
-if ($updated) {
-    echo "Student updated successfully.";
-} else {
-    echo "Failed to update student.";
 }
+*/
 
-public function deleteStudent($registrationNumber)
-{
-    $query = "DELETE FROM students WHERE registration = :registration";
-    $params = array(':registration' => $registrationNumber);
-    $success = $this->executeQuery($query, $params);
-    return $success;
-}
-
+// Ejemplo de uso GetAll
 $studentDAO = new StudentDAO();
-
-$registrationNumberToDelete = " "; // Número de registro del estudiante a eliminar
-
-$deleted = $studentDAO->deleteStudent($registrationNumberToDelete);
-
-if ($deleted) {
-    echo "Student deleted successfully.";
-} else {
-    echo "Failed to delete student.";
+$students = $studentDAO->GetAll();
+foreach ($students as $key => $student) {
+    echo "$key: {$student->name} : {$student-> p_last_name} : {$student-> status} : {$student-> birthday_date}\n";
 }
+
+/* ejemplo de SELECT */
+/*
+    $querySelect = "SELECT * FROM usuarios WHERE id = 1";
+    $resultadoSelect = $conexionDB->getData($querySelect);
+    print_r($resultadoSelect);
+*/
+
+/* ejemplo de INSERT */
+/*
+    $nombre = "Juan";
+    $email = "juan@example.com";
+    $queryInsert = "INSERT INTO usuarios (nombre, email) VALUES (?, ?)";
+    $insertId = $conexionDB->insertDataId($queryInsert, [$nombre, $email]);
+*/
+
+/* ejemplo de UPDATE */
+/*
+    $nuevoNombre = "Pedro";
+    $idActualizar = 2;
+    $queryUpdate = "UPDATE usuarios SET nombre = ? WHERE id = ?";
+    $conexionDB->updateData($queryUpdate, [$nuevoNombre, $idActualizar]);
+*/
+/* ejemplo de DELETE */
+/*
+    $idEliminar = 3;
+    $queryDelete = "DELETE FROM usuarios WHERE id = ?";
+    $conexionDB->deleteData($queryDelete, [$idEliminar]);
+*/
