3673f2c3af3d4c8cc7957a053144905e3cfc3246
ricardoMogas <al066208@uacam.mx>
2024-03-25 18:11:50 -0600

    instacia remota datos agregados

--- core/db/pfiv2.sql
+++ core/db/pfiv2.sql
@@ -7,6 +7,11 @@
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
+-- Base de datos: `pfiv2remoto321`
+-- usuario remoto: rootpfiuser321
+-- contraseña remota: pfipasskey22
+-- correo: al066208@uacam.mx
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
