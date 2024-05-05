5b5f1c920d9d5bc78d26eba1665e0814a216cae5
ricardoMogas <al066208@uacam.mx>
2024-05-05 17:59:11 -0600

    Graph Casi Completo(Genero, Etnia, Licenciatura)

--- controllers/GraphController.php
+++ controllers/GraphController.php
@@ -28,23 +28,76 @@ class GraphController extends responseData
 
         // OPTENER LOS DATOS DE LA BASE DE DATOS DIRECTAMENTE
         if ($type === "Genero") {
-            if(isset($params["startDate"]) && isset($params["endDate"])) {
+            if (isset($params["startDate"]) && isset($params["endDate"])) {
                 $startDate = $params["startDate"];
                 $endDate = $params["endDate"];
             }
-            $queryH = "SELECT COUNT(*) AS cauntity_male FROM students WHERE gender = '" . NAME_MALE . "'";
-            $queryM = "SELECT COUNT(*) AS cuantity_female FROM students WHERE gender = '" . NAME_FEMALE . "'";
+            $query = "SELECT 
+                        SUM(CASE WHEN gender = '" . NAME_MALE . "' THEN 1 ELSE 0 END) AS cauntity_male,
+                        SUM(CASE WHEN gender = '" . NAME_FEMALE . "' THEN 1 ELSE 0 END) AS cuantity_female
+                        FROM students";
             if ($startDate !== null && $endDate !== null) {
-                $queryH .= " AND date_of_registration BETWEEN '$startDate' AND '$endDate'";
-                $queryM .= " AND date_of_registration BETWEEN '$startDate' AND '$endDate'";
+                $query .= " WHERE date_of_registration BETWEEN '$startDate' AND '$endDate'";
             }
-            $resultH = $consulta->getData($queryH);
-            $resultM = $consulta->getData($queryM);
+            $result = $consulta->getData($query);
             $jsonBody = [
-                ['name' => 'Hombre', 'value' => $resultH[0]['cauntity_male']],
-                ['name' => 'Mujer', 'value' => $resultM[0]['cuantity_female']],
+                [
+                    'name' => 'Hombre', 
+                    'Total' => is_numeric($result[0]["cauntity_male"]) ? (int)$result[0]["cauntity_male"] : 0
+                ],
+                [
+                    'name' => 'Mujer', 
+                    'Total' => is_numeric($result[0]["cuantity_female"]) ? (int)$result[0]["cuantity_female"] : 0
+                ],
             ];
             return parent::sendJsonResponse("ok", $jsonBody);
+
+
+        } else if ($type === "Etnia") {
+            $etniasList = ["Otro", "Maya", "Mixteca", "Maya", "Afroamericano"];
+            $jsonBody = [];
+            foreach ($etniasList as $etnia) {
+                $query = "SELECT 
+                            SUM(CASE WHEN gender = '" . NAME_MALE . "' THEN 1 ELSE 0 END) AS cauntity_male,
+                            SUM(CASE WHEN gender = '" . NAME_FEMALE . "' THEN 1 ELSE 0 END) AS cuantity_female
+                            FROM students 
+                            WHERE ethnicity = '$etnia'";
+
+                if ($startDate !== null && $endDate !== null) {
+                    $query .= " AND date_of_registration BETWEEN '$startDate' AND '$endDate'";
+                }
+
+                $result = $consulta->getData($query);
+
+                $jsonBody[] = [
+                    'name' => $etnia,
+                    'Hombre' => is_numeric($result[0]["cauntity_male"]) ? (int)$result[0]["cauntity_male"] : 0,
+                    'Mujer' => is_numeric($result[0]["cuantity_female"]) ? (int)$result[0]["cuantity_female"] : 0
+                ];
+            }
+            return parent::sendJsonResponse("ok", $jsonBody);
+        } else if ($type === "Licenciatura") {
+            $LicenciaturasList = ["ISC", "IM", "ICA", "IE", "IME", "ITS"];
+            $jsonBody = [];
+            foreach ($LicenciaturasList as $licenciatura) {
+                $query = "SELECT 
+                SUM(CASE WHEN gender = '" . NAME_MALE . "' THEN 1 ELSE 0 END) AS cauntity_male,
+                SUM(CASE WHEN gender = '" . NAME_FEMALE . "' THEN 1 ELSE 0 END) AS cuantity_female
+                FROM students 
+                WHERE career = '$licenciatura'";
+                if ($startDate !== null && $endDate !== null) {
+                    $query .= " AND date_of_registration BETWEEN '$startDate' AND '$endDate'";
+                }
+                $result = $consulta->getData($query);
+                $jsonBody[] = [
+                    'name' => $licenciatura,
+                    'Hombre' => is_numeric($result[0]["cauntity_male"]) ? (int)$result[0]["cauntity_male"] : 0,
+                    'Mujer' => is_numeric($result[0]["cuantity_female"]) ? (int)$result[0]["cuantity_female"] : 0
+                ];
+            }
+            return parent::sendJsonResponse("ok", $jsonBody);
+        } else if ($type === "Visitas") {
+            return parent::sendJsonResponse("ok", "En proceso de desarrollo");
         }
     }
 
