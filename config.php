3b1b60725fded0fc059593771e65feef20538148
ricardoMogas <al066208@uacam.mx>
2024-05-25 10:46:15 -0600

    Config.php modify

--- config.php
+++ config.php
@@ -10,16 +10,17 @@ define('DB_NAME', 'pfiv3');
 por favor compruebe que los valores sean iguales a los de
 la tabla en la base de datos type_borrowing
 */
+//otras variables
+define('TOTAL_COPIES', 50);
+define('TOTAL_VISITAS', 5);
+
+
+
+define('NAME_MALE', 'Hombre');
+define('NAME_FEMALE', 'Mujer');
 define('computer', 1);
 define('book', 2);
 define('locker', 3);
-
 define('computer_name', "computer");
 define('book_name', "book");
 define('locker_name', "locker");
-
-
-//otras variables
-define('TOTAL_COPIES', 50);
-define('NAME_MALE', 'Hombre');
-define('NAME_FEMALE', 'Mujer');
